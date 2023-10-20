6faa757b8857a1805a543b9522382e7cd26aaf92
OlegMass <oleg.mass2015@yandex.ru>
2023-10-20 12:26:26 +0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3034e589-1a40-45db-a4d9-4de5f619d17c';
+export const MODAL_CLOSE_SEC = 2.5;
